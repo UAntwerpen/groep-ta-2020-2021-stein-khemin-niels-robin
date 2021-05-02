733301b319696958798f37b9fa631984c90663f1
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2021-05-02 14:34:20 +0200

    Gronden

--- Cell.h
+++ Cell.h
@@ -4,6 +4,9 @@
 #define TA_CELL_H
 
 #include "Lib.h"
+#include <string>
+#include <vector>
+#include "Vehicle.h"
 
 class Cell {
 public:
@@ -11,11 +14,35 @@ public:
 
     virtual EStates getState();
 
+    double getHappiness();
+
 private:
     int row;
     int col;
-    double happiness;
+
+    std::vector<Peaple*> peaple;
 };
 
+class Vegetation : public Cell{
+private:
+    //Building building;
+};
 
+class Road : public Cell{
+private:
+    std::string pixelArt;
+    std::vector<Vehicle*> vehicles;
+};
+
+class ResidentialZone : public Cell{
+    //Building building;
+};
+
+class IndustrialZone : public Cell{
+    //Building building;
+};
+
+class StoreZone : public Cell{
+    //Building building;
+};
 #endif //TA_CELL_H
