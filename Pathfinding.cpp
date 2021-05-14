dad4084cdae1863ec101eeebf6b1c24f3e4df6c5
Khemin <khemin8712@gmail.com>
2021-05-14 11:42:10 +0200

    class completion

--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -5,9 +5,11 @@
 #include "Pathfinding.h"
 
 PFMask::PFMask(CellulaireAutomaat& cellAutomaat) : width(), height() {
-    //TODO initialize attributes
+
 }
 
 bool PFMask::update() {
+
+
     return false;
 }
