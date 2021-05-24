e614e817197a6f33d07b7deeeb2f4a7a5bfdfd58
Niels <nielsvandenbroeck11@gmail.com>
2021-05-24 13:51:31 +0200

    verval + Park

--- Cell.h
+++ Cell.h
@@ -144,7 +144,7 @@ public:
     float getHappiness() const override;
 
 private:
-    std::string pixelArt = "../PixelArt/Default.png";
+    std::string pixelArt = "../PixelArt/Park.png";
 };
 
 class Road : public Cell{
